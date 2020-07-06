d28f7373943f3cbe40f555654f0ce9c9c005427c
Otilia <otilia.sindilariu@gmail.com>
2020-07-06 21:08:18 +0200

    done

--- 02-think-in-react/src/components/ProductTable.js
+++ 02-think-in-react/src/components/ProductTable.js
@@ -2,21 +2,35 @@ import React from 'react';
 import ProductCategoryRow from './ProductCategoryRow';
 import ProductRow from './ProductRow';
 
-function ProductTable({ categories, products }) {
-  const getProductsByCategory = (category) => {
-    return products.filter((product) => product.category === category);
+function ProductTable({ categories, products, filterText, isStockOnly }) {
+  const searchProduct = () => {
+    const foundProducts = products.filter(
+      (product) => product.name.indexOf(filterText) !== -1
+    );
+    return foundProducts;
   };
-  /*
-  const getRenderedProducts = () => {
-    return categories.map((category) => {
-      const productsFilteredByCategory = products.filter((product) => {
-        product.category === category;
-      });
-      return <
-    });
-  };*/
 
-  const renderedCategories = categories.map((category) => {
+  const getProductsByCategory = (category) => {
+    let categProducts;
+    if (filterText === '') {
+      categProducts = products.filter((product) => product.category === category);
+    } else {
+      const foundProducts = searchProduct();
+      categProducts = foundProducts.filter(
+        (product) => product.category === category
+      );
+    }
+    if (isStockOnly === true) {
+      return categProducts.filter((product) => product.stocked === isStockOnly);
+    }
+    return categProducts;
+  };
+
+  const getProducts = (category) => {
+    return products.map.filter((product) => product.stocked === isStockOnly);
+  };
+
+  const rendered = categories.map((category) => {
     return (
       <div>
         <ProductCategoryRow category={category} key={category}></ProductCategoryRow>
@@ -24,11 +38,12 @@ function ProductTable({ categories, products }) {
       </div>
     );
   });
+
   return (
     <div className="ui container product-table">
       ProductTable
       <p>Name | Price {categories.length}</p>
-      {renderedCategories}
+      {rendered}
     </div>
   );
 }
