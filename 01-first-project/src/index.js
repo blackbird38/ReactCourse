e024ddaa799d8b589c5fbda4fc7c4c31b7b97867
Otilia <otilia.sindilariu@gmail.com>
2020-07-06 10:44:33 +0200

    props

--- 01-first-project/src/index.js
+++ 01-first-project/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App hello="Coucou" />
   </React.StrictMode>,
   document.getElementById('root')
 );
