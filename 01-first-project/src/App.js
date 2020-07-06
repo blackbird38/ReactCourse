7b2fc01c1883ed24c01dd41a4edb33134d37c1f2
Otilia <otilia.sindilariu@gmail.com>
2020-07-06 11:56:16 +0200

    parent to child and child to parent with setState

--- 01-first-project/src/App.js
+++ 01-first-project/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Button from './Button';
 import InputText from './InputText';
 
 function App({ name }) {
+  const [value, setValue] = useState('');
   return (
     <div className="App">
-      <div>Hello, {name}!</div>
-      <InputText />
-      <Button />
+      <div>Hello, {value}!</div>
+      <InputText handleChange={setValue} value={value} />
+      <Button handleChange={setValue} />
     </div>
   );
 }
--- 01-first-project/src/Button.js
+++ 01-first-project/src/Button.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './App.css';
 
 const Button = (props) => {
-  return <button className="button">Change name</button>;
+  function onHandleChange(e) {
+    props.handleChange('Jon Doe');
+  }
+  return (
+    <button className="button" onClick={onHandleChange}>
+      Get anonymous!
+    </button>
+  );
 };
 
 export default Button;
